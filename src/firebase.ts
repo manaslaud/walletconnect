b48ab91ee9b9a4ec3cf1e6a68fbb80bf2ed7c882
manaslaud <manaslaud@hotmail.com>
2024-05-31 02:40:25 +0530

    init env

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,22 +1,24 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import {getAuth} from "firebase/auth"
+import { getDatabase } from "firebase/database";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.FIREBASE_API_KEY,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "web3-auth-50e10.firebaseapp.com",
   projectId: "web3-auth-50e10",
   storageBucket: "web3-auth-50e10.appspot.com",
   messagingSenderId: "638110540546",
-  appId: import.meta.env.FIREBASE_APP_ID,
+  appId:import.meta.env.VITE_FIREBASE_APP_ID,
   measurementId: "G-F9S9F45DBL"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth =getAuth(app)
-export {app,auth}
+const database=getDatabase(app)
+export {app,auth,database}
